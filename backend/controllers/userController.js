f1e34ec3c04a3f87865965d7a90a8fcd5675c9ab
George Chirikov <george.chirikov@metropolia.fi>
2024-03-15 10:52:41 +0200

    add status codes

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,44 +2,72 @@ const User = require('../models/userModel');
 const jwt = require('jsonwebtoken');
 
 const createToken = (_id) => {
-  return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
+	return jwt.sign({ _id }, process.env.SECRET, { expiresIn: '3d' });
 };
 
 // log in an existing user
 const loginUser = async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password } = req.body;
 
-  try {
-    const user = await User.login(email, password);
+	if (!email || !password) {
+		return res.status(400).json({
+			error: 'Please provide email and password.',
+		});
+	}
 
-    // create a token
-    const token = createToken(user._id);
+	if (
+		email != User.findOne({ email }) ||
+		password != User.findOne({ password })
+	) {
+		return res.status(400).json({
+			error: 'Email or password is incorrect',
+		});
+	}
 
-    res.status(200).json({ email, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+	try {
+		const user = await User.login(email, password);
+
+		// create a token
+		const token = createToken(user._id);
+
+		res.status(200).json({ email, token });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 // Signin an new user
 const signupUser = async (req, res) => {
-  const { email, password } = req.body;
+	const { email, password, firstName, lastName, phoneNumber, role } = req.body;
 
-  try {
-    const user = await User.signup(email, password);
+	if (!email || !password) {
+		return res.status(400).json({
+			error: 'Please provide email and password.',
+		});
+	}
 
-    // create a token
-    const token = createToken(user._id);
+	try {
+		const user = await User.signup(
+			email,
+			password,
+			firstName,
+			lastName,
+			phoneNumber,
+			role
+		);
 
-    res.status(201).json({ email, token });
-  } catch (error) {
-    res.status(400).json({ error: error.message });
-  }
+		// create a token
+		const token = createToken(user._id);
+
+		res.status(201).json({ email, token });
+	} catch (error) {
+		res.status(500).json({ error: error.message });
+	}
 };
 
 const getUsers = async (req, res) => {
-  const users = await User.find();
-  res.status(200).json(users);
+	const users = await User.find();
+	res.status(200).json(users);
 };
 
 module.exports = { signupUser, loginUser, getUsers };
